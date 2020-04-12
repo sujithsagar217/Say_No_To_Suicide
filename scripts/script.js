bfc0cd01bba6111f74c918861519f1ccb1d65a38
Sujith <gdvsujithsagar217@gmail.com>
2020-04-12 16:14:33 +0530

    added registration and login frontend

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+/*start user registration slider*/
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
+  console.log("hello");
+
+/*user registration slider end*/
