5cdb0138c30598b055c24afcbb0b53c0ce323fca
Sujith <gdvsujithsagar217@gmail.com>
2020-04-12 17:20:39 +0530

    login and register in same page

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log("hello");
 
 /*user registration slider end*/
+
+/*login user*/
+
+function login_user(){
+  console.log("called login_user function");
+  var form = document.getElementById("login");
+  form.style.display="block";
+  form = document.getElementById("register");
+  form.style.display="none";
+}
+
+/*register user*/
+
+function register_user(){
+  console.log("called register_user function");
+  var form = document.getElementById("login");
+  form.style.display="none";
+  form = document.getElementById("register");
+  form.style.display="block";
+}
